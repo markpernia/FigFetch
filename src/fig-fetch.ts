12685b39dcdb52e0fd62de734f44642e9078dc6e
markpernia <perniamanila@gmail.com>
2025-07-16 21:53:30 +0200

    clean up file names

---
+++ src/fig-fetch.ts
@@ -0,0 +1,4 @@
+//  figma-api.ts (responsible for api calls)
+//  file-handler.ts (responsible for overwrite file logic etc...)
+//  figma-download.ts (responsible for automation)
+//  fig-fetch.ts (responsible for the batch download logic)
