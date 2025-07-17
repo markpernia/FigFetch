c44ac631052681c0e90d33fca311c307d7d855f0
markpernia <perniamanila@gmail.com>
2025-07-17 21:44:30 +0200

    getting organized

--- src/modules/fig-fetch.ts
+++
@@ -1,4 +0,0 @@
-//  figma-api.ts (responsible for api calls)
-//  file-handler.ts (responsible for overwrite file logic etc...)
-//  figma-download.ts (responsible for automation)
-//  fig-fetch.ts (responsible for the batch download logic)
